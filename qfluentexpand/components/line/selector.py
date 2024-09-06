5211db697faec0cd41e83c0172091a42559d7936
Reinering <nbxlhc@hotmail.com>
2024-09-06 20:46:01 +0800

    add selector

---
+++ qfluentexpand/components/line/selector.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+author: Reiner New
+email: nbxlc@hotmail.com
+"""
+
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QFileDialog
+
+from qfluentwidgets.components.widgets.line_edit import LineEdit, LineEditButton
+from qfluentwidgets.common.icon import FluentIcon as FIF
+
+
+class FilePathSelector(LineEdit):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setPlaceholderText("Select a file path")
+        self.setReadOnly(True)
+        self.fileTypes = "*"
+
+        self.dropButton = LineEditButton(FIF.RIGHT_ARROW, self)
+        self.dropButton.setFixedSize(30, 25)
+        self.dropButton.clicked.connect(self._toggleSelect)
+        self.hBoxLayout.addWidget(self.dropButton, 0, Qt.AlignRight)
+
+    def setFileTypes(self, fileTypes):
+        self.fileTypes = fileTypes
+
+    def _toggleSelect(self):
+        try:
+            filePath = QFileDialog.getOpenFileName(self, u"选择文件", "/",
+                                                   self.fileTypes)
+            if not filePath[0]:
+                return
+
+            self.setText(filePath[0])
+        except Exception as e:
+            print(e)
+
+
+class FolderPathSelector(LineEdit):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setPlaceholderText("Select a file folder")
+        self.setReadOnly(True)
+
+        self.dropButton = LineEditButton(FIF.RIGHT_ARROW, self)
+        self.dropButton.setFixedSize(30, 25)
+        self.dropButton.clicked.connect(self._toggleSelect)
+        self.hBoxLayout.addWidget(self.dropButton, 0, Qt.AlignRight)
+
+    def _toggleSelect(self):
+        try:
+            folderPath = QFileDialog.getExistingDirectory(self, u"选择目录", "/",
+                                                          QFileDialog.ShowDirsOnly)
+            if not folderPath:
+                return
+
+            self.setText(folderPath)
+        except Exception as e:
+            print(e)
+
