0d8b7832b9ac81d0bf488321bb29df1d563284c3
Reinering <nbxlhc@hotmail.com>
2024-09-09 21:01:40 +0800

    add MSCombobox

--- qfluentexpand/components/combox/combo_box.py
+++ qfluentexpand/components/combox/combo_box.py
@@ -26,6 +26,271 @@ from qfluentwidgets.components.widgets.combo_box import ComboBoxBase, ComboItem
 from .base import Line
 
 
+class MSComboBox(Line, ComboBoxBase):
+    """ Multi Selection combo box """
+
+    currentIndexChanged = Signal(int)
+    currentTextChanged = Signal(str)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self._setUpUi()
+        self.setTextMargins(0, 0, 29, 0)
+
+        self.dropButton = LineEditButton(FIF.ARROW_DOWN, self)
+        self.dropButton.setFixedSize(30, 25)
+        self.dropButton.clicked.connect(self._toggleComboMenu)
+        self.hBoxLayout.addWidget(self.dropButton, 0, Qt.AlignRight)
+
+        self.textChanged.connect(self._onComboTextChanged)
+        self.returnPressed.connect(self._onReturnPressed)
+        FluentStyleSheet.LINE_EDIT.apply(self)
+
+        self.clearButton.clicked.disconnect()
+        self.clearButton.clicked.connect(self._onClearButtonClicked)
+
+        self.items = []
+        #
+        self.selectedItems = []     # 被选择的index
+        self.selectedWidgets = []   # del button
+        self.widgets = []   # 下拉列表
+
+        self.rowSize = 1
+
+    def setCompleterMenu(self, menu):
+        super().setCompleterMenu(menu)
+
+    def setRowSize(self, size: int):
+        if size > 0 and size <= len(self.items):
+            self.rowSize = size
+
+    def currentText(self):
+        return self.text()
+
+    def selectedTexts(self) -> str:
+        tmp = []
+        for i in self.selectedItems:
+            tmp.append(self.items[i].text)
+        return tmp
+
+    def setPlaceholderText(self, text: str):
+        self._placeholderText = text
+        super().setPlaceholderText(text)
+
+    def eventFilter(self, obj, e: QEvent):
+        if obj is self:
+            if e.type() == QEvent.MouseButtonPress:
+                self.isPressed = True
+            elif e.type() == QEvent.MouseButtonRelease:
+                self.isPressed = False
+            elif e.type() == QEvent.Enter:
+                self.isHover = True
+            elif e.type() == QEvent.Leave:
+                self.isHover = False
+
+        return super().eventFilter(obj, e)
+
+    def addItem(self, text, icon: Union[str, QIcon, FluentIconBase] = None, userData=None):
+        item = ComboItem(text, icon, userData)
+        self.items.append(item)
+
+    def addItems(self, texts: Iterable[str]):
+        self.clear()
+        for text in texts:
+            self.addItem(text)
+
+    def insertItem(self, index: int, text: str, icon: Union[str, QIcon, FluentIconBase] = None, userData=None):
+        item = ComboItem(text, icon, userData)
+        self.items.insert(index, item)
+
+    def removeItem(self, index: int):
+        pass
+
+    def itemData(self, index):
+        text = None
+        try:
+            text = self.widgets[index][1].currentText()
+        except Exception as e:
+            print(e)
+        return text
+
+    def itemDatas(self):
+        return [self.itemData(index) for index in self.selectedItems]
+
+    def clear(self):
+        """ Clears the combobox, removing all items. """
+
+        self.items.clear()
+        self.selectedItems.clear()
+        self.widgets.clear()
+
+        if self.isReadOnly():
+            for widget in self.selectedWidgets:
+                self.hBoxLayout.removeItem(widget)
+                widget.deleteLater()
+            self.selectedWidgets.clear()
+
+        super().setPlaceholderText(self._placeholderText)
+
+    # 下拉按钮点击事件
+    def _toggleComboMenu(self, event):
+        if self.dropMenu:
+            self._closeComboMenu()
+        else:
+            self._showComboMenu()
+
+    # 下拉按钮关闭事件
+    def _onDropMenuClosed(self):
+        if sys.platform != "win32":
+            self.dropMenu = None
+        else:
+            pos = self.mapFromGlobal(QCursor.pos())
+            if not self.rect().contains(pos):
+                self.dropMenu = None
+
+    def _closeComboMenu(self):
+        if not self.dropMenu:
+            return
+
+        self.dropMenu.close()
+        self.dropMenu = None
+
+    # 生成下拉菜单显示，并绑定事件
+    def _showComboMenu(self):
+        if not self.items:
+            return
+
+        menu = self._createComboMenu()
+        self.widgets.clear()
+        for i, item in enumerate(self.items):
+            if i % self.rowSize == 0:
+                tmpWidget = QWidget(menu)
+                hBoxLayout = QHBoxLayout(tmpWidget)
+                hBoxLayout.setSpacing(1)
+                hBoxLayout.setContentsMargins(1, 1, 1, 1)
+                menu.addWidget(tmpWidget)
+
+            checkbox = CheckBox()
+            # checkbox.setMaximumSize(29, 20)
+            checkbox.setMaximumHeight(20)
+            checkbox.setObjectName("Checkbox_C_" + str(i))
+
+            if i in self.selectedItems:
+                checkbox.setChecked(True)
+            else:
+                checkbox.setChecked(False)
+
+            if item.text:
+                checkbox.setText(item.text)
+
+            checkbox.stateChanged.connect(lambda state, index=i: self._onItemChecked(state, index))
+            hBoxLayout.addWidget(checkbox)
+
+            tmpWidget.resize(menu.width(), 45)
+
+            self.widgets.append(checkbox)
+
+        if menu.view.width() < self.width():
+            menu.view.setMinimumWidth(self.width())
+            menu.adjustSize()
+
+        menu.setMaxVisibleItems(self.maxVisibleItems())
+        menu.closedSignal.connect(self._onDropMenuClosed)
+        self.dropMenu = menu
+
+        x = -menu.width() // 2 + menu.layout().contentsMargins().left() + self.width() // 2
+        pd = self.mapToGlobal(QPoint(x, self.height()))
+        hd = menu.view.heightForAnimation(pd, MenuAnimationType.DROP_DOWN)
+
+        pu = self.mapToGlobal(QPoint(x, 0))
+        hu = menu.view.heightForAnimation(pu, MenuAnimationType.PULL_UP)
+
+        if hd >= hu:
+            menu.view.adjustSize(pd, MenuAnimationType.DROP_DOWN)
+            menu.exec(pd, aniType=MenuAnimationType.DROP_DOWN)
+        else:
+            menu.view.adjustSize(pu, MenuAnimationType.PULL_UP)
+            menu.exec(pu, aniType=MenuAnimationType.PULL_UP)
+
+    def _onItemTextChanged(self, text, index):
+        self.items[index].userData = text
+
+    def _onItemChecked(self, checked, index):
+        checked = self.widgets[index].isChecked()
+        if checked:
+            self.widgets[int(index)].setEnabled(True)
+
+            self.selectedItems.append(index)
+            if self.isReadOnly():
+                self._addDeleteButton(index, self.items[int(index)].text)
+                if len(self.selectedItems) == 1:
+                    super().setPlaceholderText('')
+        else:
+            self.widgets[int(index)].setEnabled(False)
+
+            self.selectedItems.remove(index)
+            if self.isReadOnly():
+                delButton = self.findChild(PushButton, "DeleteButton_C_" + str(index))
+                if delButton:
+                    self._removeDelButton(delButton)
+                if len(self.selectedItems) == 0:
+                    super().setPlaceholderText(self._placeholderText)
+
+    def _addDeleteButton(self, index, text):
+        delButton = PushButton(FIF.CLOSE, text)
+        delButton.setObjectName("DeleteButton_C_" + str(index))
+        delButton.clicked.connect(lambda check, index=index, text=text: self._onDeleteButtonClicked(index, text))
+
+        insert_position = 0
+        for i, widget in enumerate(self.selectedWidgets):
+            current_index = int(widget.objectName().split('_')[-1])
+            if current_index > index:
+                break
+            insert_position = i + 1
+
+        self.hBoxLayout.insertWidget(insert_position, delButton, 0, Qt.AlignLeft)
+        self.selectedWidgets.insert(insert_position, delButton)
+
+    def _removeDelButton(self, widget):
+        self.selectedWidgets.remove(widget)
+        self.hBoxLayout.removeWidget(widget)
+        widget.deleteLater()
+
+    def _onDeleteButtonClicked(self, index, text):
+        self.widgets[int(index)].setEnabled(False)
+
+        delButton = self.findChild(PushButton, "DeleteButton_C_" + str(index))
+        if delButton:
+            self._removeDelButton(delButton)
+            self.selectedItems.remove(index)
+        if len(self.selectedWidgets) == 0:
+            super().setPlaceholderText(self._placeholderText)
+
+    def _onReturnPressed(self):
+        if self.isReadOnly() or not self.text():
+            return
+
+        index = self.findText(self.text())
+        if index >= 0 and index != self.currentIndex():
+            self._currentIndex = index
+            self.currentIndexChanged.emit(index)
+        elif index == -1:
+            self.addItem(self.text())
+            self.setCurrentIndex(self.count() - 1)
+
+    def _onComboTextChanged(self, text: str):
+        if self.isReadOnly():
+            return
+
+        self.currentTextChanged.emit(text)
+
+    def _onClearButtonClicked(self):
+        if self.isReadOnly():
+            self.clear()
+        else:
+            LineEdit.clear(self)
+
+
 class MSEComboBox(Line, ComboBoxBase):
     """ Multi Selection Editable combo box """
 
@@ -55,8 +320,13 @@ class MSEComboBox(Line, ComboBoxBase):
         self.selectedWidgets = []
         self.widgets = []
 
+        self.rowSize = 1
+
     def setCompleterMenu(self, menu):
         super().setCompleterMenu(menu)
+
+    def setRowSize(self, size):
+        self.rowSize = size
 
     def currentText(self):
         return self.text()
@@ -150,12 +420,14 @@ class MSEComboBox(Line, ComboBoxBase):
 
         menu = self._createComboMenu()
         self.widgets.clear()
+
         for i, item in enumerate(self.items):
             tmpWidget = QWidget(menu)
 
-            hBoxLayout = QHBoxLayout(tmpWidget)
-            hBoxLayout.setSpacing(1)
-            hBoxLayout.setContentsMargins(1, 1, 1, 1)
+            if i % self.rowSize == 0:
+                hBoxLayout = QHBoxLayout(tmpWidget)
+                hBoxLayout.setSpacing(1)
+                hBoxLayout.setContentsMargins(1, 1, 1, 1)
 
             checkbox = CheckBox()
             checkbox.setMaximumSize(29, 20)
