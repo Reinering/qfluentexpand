5e8e6616cee55b8a13a28c2506baf6fbdac92e41
Reinering <nbxlhc@hotmail.com>
2024-11-17 17:32:11 +0800

    repair addSubInterface routeKey is null

--- qfluentexpand/window/fluent_window.py
+++ qfluentexpand/window/fluent_window.py
@@ -6,10 +6,11 @@ email: nbxlc@hotmail.com
 """
 
 
-from PySide6.QtWidgets import QWidget
-from PySide6.QtGui import QPainter, QCursor, QIcon, QAction, QFont
-from PySide6.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QLabel, QApplication
-from typing import Union
+from typing import Union, List, Dict, Any, Optional
+import sys
+from PySide6.QtCore import QSize, QRect
+from PySide6.QtGui import QPainter, QCursor, QIcon, QAction, QFont, Qt, QColor
+from PySide6.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QLabel, QApplication, QToolBar, QGridLayout, QDockWidget
 
 from qfluentwidgets.window.fluent_window import FluentWindowBase, FluentTitleBar
 from qfluentwidgets.common.icon import FluentIcon as FIF
@@ -18,8 +19,7 @@ from qfluentwidgets.common.config import qconfig
 from qfluentwidgets.common.router import qrouter
 from qfluentwidgets.common.style_sheet import FluentStyleSheet, isDarkTheme, setTheme, Theme
 from qfluentwidgets.common.animation import BackgroundAnimationWidget
-from qfluentwidgets.components.widgets.frameless_window import FramelessWindow
-from qfluentwidgets.components.navigation import ( NavigationBar, NavigationItemPosition,
+from qfluentwidgets.components.navigation import (NavigationBar, NavigationItemPosition,
                                      NavigationBarPushButton, NavigationTreeWidget)
 from qfluentwidgets.window.stacked_widget import StackedWidget
 from qfluentwidgets.common.icon import isDarkTheme, FluentIconBase
@@ -28,13 +28,62 @@ from qframelesswindow import TitleBar, TitleBarBase
 
 from qfluentexpand.components.navigation.navigation_interface import NavigationInterface
 
+from .base import FramelessMainWindow as MainWindow, FramelessDialog as Dialog, FramelessWindow as Window
 
 
-class FluentWindow(FluentWindowBase):
-    """ Fluent window """
+class FramelessMainWindow(MainWindow):
 
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.setContentsMargins(1, 35, 1, 1)
+        self.setTitleBar(FluentTitleBar(self))
+
+        self.titleBar.raise_()
+
+    def setTitleBar(self, titleBar):
+        super().setTitleBar(titleBar)
+
+        # hide title bar buttons on macOS
+        if sys.platform == "darwin" and self.isSystemButtonVisible() and isinstance(titleBar, TitleBarBase):
+            titleBar.minBtn.hide()
+            titleBar.maxBtn.hide()
+            titleBar.closeBtn.hide()
+
+    def resizeEvent(self, e):
+        self.titleBar.move(5, 0)
+        self.titleBar.resize(self.width()-5, self.titleBar.height())
+
+
+class FluentMainWindow(BackgroundAnimationWidget, MainWindow):
+    """ Fluent window base class """
+
+    def __init__(self, parent=None):
+        self._isMicaEnabled = False
+        self._lightBackgroundColor = QColor(240, 244, 249)
+        self._darkBackgroundColor = QColor(32, 32, 32)
+        super().__init__(parent=parent)
+
+        self.setContentsMargins(1, 48, 1, 1)
+
+        self.hBoxLayout = QHBoxLayout(self)
+        self.stackedWidget = StackedWidget(self)
+        self.navigationInterface = None
+
+        # initialize layout
+        self.hBoxLayout.setSpacing(0)
+        self.hBoxLayout.setContentsMargins(0, 0, 0, 0)
+
+        FluentStyleSheet.FLUENT_WINDOW.apply(self.stackedWidget)
+
+        # enable mica effect on win11
+        self.setMicaEffectEnabled(True)
+
+        # show system title bar buttons on macOS
+        if sys.platform == "darwin":
+            self.setSystemTitleBarButtonVisible(True)
+
+        qconfig.themeChangedFinished.connect(self._onThemeChangedFinished)
+
         self.setTitleBar(FluentTitleBar(self))
         self.interfaceKeys = {}
 
@@ -108,6 +157,90 @@ class FluentWindow(FluentWindowBase):
 
         return item
 
+
+    def switchTo(self, interface: QWidget):
+        self.stackedWidget.setCurrentWidget(interface, popOut=False)
+
+    def _onCurrentInterfaceChanged(self, index: int):
+        widget = self.stackedWidget.widget(index)
+        self.navigationInterface.setCurrentItem(widget.objectName())
+        qrouter.push(self.stackedWidget, widget.objectName())
+
+        self._updateStackedBackground()
+
+    def _updateStackedBackground(self):
+        isTransparent = self.stackedWidget.currentWidget().property("isStackedTransparent")
+        if bool(self.stackedWidget.property("isTransparent")) == isTransparent:
+            return
+
+        self.stackedWidget.setProperty("isTransparent", isTransparent)
+        self.stackedWidget.setStyle(QApplication.style())
+
+    def setCustomBackgroundColor(self, light, dark):
+        """ set custom background color
+
+        Parameters
+        ----------
+        light, dark: QColor | Qt.GlobalColor | str
+            background color in light/dark theme mode
+        """
+        self._lightBackgroundColor = QColor(light)
+        self._darkBackgroundColor = QColor(dark)
+        self._updateBackgroundColor()
+
+    def _normalBackgroundColor(self):
+        if not self.isMicaEffectEnabled():
+            return self._darkBackgroundColor if isDarkTheme() else self._lightBackgroundColor
+
+        return QColor(0, 0, 0, 0)
+
+    def _onThemeChangedFinished(self):
+        if self.isMicaEffectEnabled():
+            self.windowEffect.setMicaEffect(self.winId(), isDarkTheme())
+
+    def paintEvent(self, e):
+        super().paintEvent(e)
+        painter = QPainter(self)
+        painter.setPen(Qt.NoPen)
+        painter.setBrush(self.backgroundColor)
+        painter.drawRect(self.rect())
+
+    def setMicaEffectEnabled(self, isEnabled: bool):
+        """ set whether the mica effect is enabled, only available on Win11 """
+        if sys.platform != 'win32' or sys.getwindowsversion().build < 22000:
+            return
+
+        self._isMicaEnabled = isEnabled
+
+        if isEnabled:
+            self.windowEffect.setMicaEffect(self.winId(), isDarkTheme())
+        else:
+            self.windowEffect.removeBackgroundEffect(self.winId())
+
+        self.setBackgroundColor(self._normalBackgroundColor())
+
+    def isMicaEffectEnabled(self):
+        return self._isMicaEnabled
+
+    def systemTitleBarRect(self, size: QSize) -> QRect:
+        """ Returns the system title bar rect, only works for macOS
+
+        Parameters
+        ----------
+        size: QSize
+            original system title bar rect
+        """
+        return QRect(size.width() - 75, 0 if self.isFullScreen() else 9, 75, size.height())
+
+    def setTitleBar(self, titleBar):
+        super().setTitleBar(titleBar)
+
+        # hide title bar buttons on macOS
+        if sys.platform == "darwin" and self.isSystemButtonVisible() and isinstance(titleBar, TitleBarBase):
+            titleBar.minBtn.hide()
+            titleBar.maxBtn.hide()
+            titleBar.closeBtn.hide()
+
     def forward(self, text: str):
         """ switch to the interface by text """
         if self.interfaceKeys.get(text):
@@ -116,3 +249,122 @@ class FluentWindow(FluentWindowBase):
     def resizeEvent(self, e):
         self.titleBar.move(46, 0)
         self.titleBar.resize(self.width()-46, self.titleBar.height())
+
+#
+class FluentWindow(FluentWindowBase):
+    """ Fluent window """
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setTitleBar(FluentTitleBar(self))
+        self.interfaceKeys = {}
+
+        self.navigationInterface = NavigationInterface(self, showReturnButton=True)
+        self.widgetLayout = QHBoxLayout()
+
+        # initialize layout
+        self.hBoxLayout.addWidget(self.navigationInterface)
+        self.hBoxLayout.addLayout(self.widgetLayout)
+        self.hBoxLayout.setStretchFactor(self.widgetLayout, 1)
+
+        self.widgetLayout.addWidget(self.stackedWidget)
+        self.widgetLayout.setContentsMargins(0, 40, 0, 0)
+
+        self.navigationInterface.displayModeChanged.connect(self.titleBar.raise_)
+        self.titleBar.raise_()
+
+    def addSubInterface(self, interface: QWidget, icon: Union[FluentIconBase, QIcon, str], text: str,
+                        position=NavigationItemPosition.TOP, parent=None, isTransparent=False) -> NavigationTreeWidget:
+        """ add sub interface, the object name of `interface` should be set already
+        before calling this method
+
+        Parameters
+        ----------p
+        interface: QWidget
+            the subinterface to be added
+
+        icon: FluentIconBase | QIcon | str
+            the icon of navigation item
+
+        text: str
+            the text of navigation item
+
+        position: NavigationItemPosition
+            the position of navigation item
+
+        parent: QWidget
+            the parent of navigation item
+
+        isTransparent: bool
+            whether to use transparent background
+        """
+        if not interface.objectName():
+            raise ValueError("The object name of `interface` can't be empty string.")
+        if parent and not parent.objectName():
+            raise ValueError("The object name of `parent` can't be empty string.")
+
+        interface.setProperty("isStackedTransparent", isTransparent)
+        self.stackedWidget.addWidget(interface)
+        self.interfaceKeys[text] = interface
+
+        # add navigation item
+        if text:
+            routeKey = text
+        else:
+            routeKey = interface.objectName()
+        item = self.navigationInterface.addItem(
+            routeKey=routeKey,
+            icon=icon,
+            text=text,
+            onClick=lambda: self.switchTo(interface),
+            position=position,
+            tooltip=text,
+            parentRouteKey=parent.objectName() if parent else None
+        )
+
+        # initialize selected item
+        if self.stackedWidget.count() == 1:
+            self.stackedWidget.currentChanged.connect(self._onCurrentInterfaceChanged)
+            self.navigationInterface.setCurrentItem(routeKey)
+            qrouter.setDefaultRouteKey(self.stackedWidget, routeKey)
+
+        self._updateStackedBackground()
+
+        return item
+
+    def forward(self, text: str):
+        """ switch to the interface by text """
+        if self.interfaceKeys.get(text):
+            self.switchTo(self.interfaceKeys[text])
+
+    def resizeEvent(self, e):
+        self.titleBar.move(46, 0)
+        self.titleBar.resize(self.width()-46, self.titleBar.height())
+
+
+class FramelessWindow(Window):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setContentsMargins(1, 35, 1, 1)
+
+
+    def addToolBar(self, toolbar: Optional['QToolBar']) -> None:
+        toolbar.setMovable(True)  # 允许工具栏移动
+        toolbar.setFloatable(True)  # 允许工具栏浮动
+        self.gridLayout.addWidget(toolbar)
+
+    def resizeEvent(self, e):
+        self.titleBar.move(5, 0)
+        self.titleBar.resize(self.width()-5, self.titleBar.height())
+
+
+class FramelessDialog(Dialog):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setContentsMargins(1, 35, 1, 1)
+
+    def resizeEvent(self, e):
+        self.titleBar.move(5, 0)
+        self.titleBar.resize(self.width()-5, self.titleBar.height())
