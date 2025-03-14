d6d5c04bb0fe4633275274d567f7e7ea686e60ad
Reinering <nbxlhc@hotmail.com>
2025-03-14 15:59:41 +0800

    FluentWindowBase add statusbar

--- qfluentexpand/window/fluent_window.py
+++ qfluentexpand/window/fluent_window.py
@@ -31,6 +31,133 @@ from qfluentexpand.components.navigation.navigation_interface import NavigationI
 from .base import FramelessMainWindow as MainWindow, FramelessDialog as Dialog, FramelessWindow as Window
 
 
+class FluentWindowBase(BackgroundAnimationWidget, Window):
+    """ Fluent window base class """
+
+    def __init__(self, parent=None):
+        self._isMicaEnabled = False
+        self._lightBackgroundColor = QColor(240, 244, 249)
+        self._darkBackgroundColor = QColor(32, 32, 32)
+        super().__init__(parent=parent)
+
+        self.gridLayout_main = QGridLayout(self)
+        self.gridLayout_main.setSpacing(1)
+        self.gridLayout_main.setObjectName(u"gridLayout_main")
+        self.gridLayout_main.setContentsMargins(0, 0, 0, 0)
+
+        self.hBoxLayout = QHBoxLayout(self)
+        self.gridLayout_main.addLayout(self.hBoxLayout, 0, 0, 1, 1)
+        self.stackedWidget = StackedWidget(self)
+        self.navigationInterface = None
+
+        # initialize layout
+        self.hBoxLayout.setSpacing(0)
+        self.hBoxLayout.setContentsMargins(0, 0, 0, 0)
+
+        FluentStyleSheet.FLUENT_WINDOW.apply(self.stackedWidget)
+
+        # enable mica effect on win11
+        self.setMicaEffectEnabled(True)
+
+        # show system title bar buttons on macOS
+        if sys.platform == "darwin":
+            self.setSystemTitleBarButtonVisible(True)
+
+        qconfig.themeChangedFinished.connect(self._onThemeChangedFinished)
+
+    def addSubInterface(self, interface: QWidget, icon: Union[FluentIconBase, QIcon, str], text: str,
+                        position=NavigationItemPosition.TOP):
+        """ add sub interface """
+        raise NotImplementedError
+
+    def switchTo(self, interface: QWidget):
+        self.stackedWidget.setCurrentWidget(interface, popOut=False)
+
+    def _onCurrentInterfaceChanged(self, index: int):
+        widget = self.stackedWidget.widget(index)
+        self.navigationInterface.setCurrentItem(widget.objectName())
+        qrouter.push(self.stackedWidget, widget.objectName())
+
+        self._updateStackedBackground()
+
+    def _updateStackedBackground(self):
+        isTransparent = self.stackedWidget.currentWidget().property("isStackedTransparent")
+        if bool(self.stackedWidget.property("isTransparent")) == isTransparent:
+            return
+
+        self.stackedWidget.setProperty("isTransparent", isTransparent)
+        self.stackedWidget.setStyle(QApplication.style())
+
+    def setCustomBackgroundColor(self, light, dark):
+        """ set custom background color
+
+        Parameters
+        ----------
+        light, dark: QColor | Qt.GlobalColor | str
+            background color in light/dark theme mode
+        """
+        self._lightBackgroundColor = QColor(light)
+        self._darkBackgroundColor = QColor(dark)
+        self._updateBackgroundColor()
+
+    def _normalBackgroundColor(self):
+        if not self.isMicaEffectEnabled():
+            return self._darkBackgroundColor if isDarkTheme() else self._lightBackgroundColor
+
+        return QColor(0, 0, 0, 0)
+
+    def _onThemeChangedFinished(self):
+        if self.isMicaEffectEnabled():
+            self.windowEffect.setMicaEffect(self.winId(), isDarkTheme())
+
+    def paintEvent(self, e):
+        super().paintEvent(e)
+        painter = QPainter(self)
+        painter.setPen(Qt.NoPen)
+        painter.setBrush(self.backgroundColor)
+        painter.drawRect(self.rect())
+
+    def setMicaEffectEnabled(self, isEnabled: bool):
+        """ set whether the mica effect is enabled, only available on Win11 """
+        if sys.platform != 'win32' or sys.getwindowsversion().build < 22000:
+            return
+
+        self._isMicaEnabled = isEnabled
+
+        if isEnabled:
+            self.windowEffect.setMicaEffect(self.winId(), isDarkTheme())
+        else:
+            self.windowEffect.removeBackgroundEffect(self.winId())
+
+        self.setBackgroundColor(self._normalBackgroundColor())
+
+    def isMicaEffectEnabled(self):
+        return self._isMicaEnabled
+
+    def systemTitleBarRect(self, size: QSize) -> QRect:
+        """ Returns the system title bar rect, only works for macOS
+
+        Parameters
+        ----------
+        size: QSize
+            original system title bar rect
+        """
+        return QRect(size.width() - 75, 0 if self.isFullScreen() else 9, 75, size.height())
+
+    def setTitleBar(self, titleBar):
+        super().setTitleBar(titleBar)
+
+        # hide title bar buttons on macOS
+        if sys.platform == "darwin" and self.isSystemButtonVisible() and isinstance(titleBar, TitleBarBase):
+            titleBar.minBtn.hide()
+            titleBar.maxBtn.hide()
+            titleBar.closeBtn.hide()
+
+    def setStatusBar(self, statusBar):
+        """ set status bar """
+        self.gridLayout_main.addWidget(statusBar, 1, 0, 1, 1)
+
+
 class FramelessMainWindow(MainWindow):
 
     def __init__(self, parent=None):
@@ -352,7 +479,10 @@ class FramelessWindow(Window):
     def addToolBar(self, toolbar: Optional['QToolBar']) -> None:
         toolbar.setMovable(True)  # 允许工具栏移动
         toolbar.setFloatable(True)  # 允许工具栏浮动
-        self.gridLayout.addWidget(toolbar)
+        try:
+            self.gridLayout.addWidget(toolbar)
+        except Exception as e:
+            print(e)
 
     def resizeEvent(self, e):
         self.titleBar.move(5, 0)
